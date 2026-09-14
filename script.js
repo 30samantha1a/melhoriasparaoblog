13acc0c0ac44260bac07708d6b6d5c2ab75b0d71
30samantha1a <samantha.rosa.rocha@escola.pr.gov.br>
2026-09-14 19:56:07 -0300

    if tema-escuro

--- script.js
+++ script.js
@@ -21,5 +21,10 @@ const btnTemaEscuro = document.querySelector("button");
 btnTemaEscuro.addEventListener('click', botaoClicado);
 
 function mudaTema(){
-  
+  const corpoPagina = document.body;
+  if (corpoPagina.classList.contains("tema-escuro")){
+    corpoPagina.classList.remove("tema-escuro");
+  } else {
+    corpoPagina.classList.add("tema-escuro");
+  }
 }
